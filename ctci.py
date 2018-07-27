89a4a3bd4189e0015bb8976aeaa9b76c18ec81c0
Shreyas <shosahal@asu.edu>
2018-07-26 20:57:22 -0700

    Given a string, function to check if it is a permutation of palindrome is added

--- ctci.py
+++ ctci.py
@@ -1,3 +1,22 @@
 class Solution:
     def checkPermutationOfPalindrome(self, str):
-        pass
+        charDict = dict()
+        for char in str:
+            if not char in charDict:
+                charDict[char] = 1
+            else:
+                charDict[char] += 1
+        # print(charDict)
+        flag = 0
+        for char in charDict:
+            if charDict[char]%2 !=0:
+                flag += 1
+        if flag == 1:
+            return True
+        return False
+
+
+test = Solution()
+
+ans = test.checkPermutationOfPalindrome('aaaaa')
+print(ans)
